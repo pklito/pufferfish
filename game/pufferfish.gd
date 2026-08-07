5632775cc91869dbdaefee65d97016bb29667455
Paulo Khayat <paulokhayat@gmail.com>
2026-08-07 13:46:50 -0400

    Make puffer buffer work

--- game/pufferfish.gd
+++ game/pufferfish.gd
@@ -1,47 +1,31 @@
 extends Node
 
 @export var amount : int = 20
-@export var targetCore : PhysicsBody2D
+@export var targetCore : Node2D
 var sbnode : PackedScene = preload("res://game/SoftBodyNode.tscn")
 # Called when the node enters the scene tree for the first time.
 
-var listRing := []
-var listRadial := []
-
-@export var ringDistance := 16.0
-@export var radialDistance := 50.0
+var listPoints := []
 
 func _ready() -> void:
 	for a in targetCore.get_children():
 		a.scale = Vector2(0.2,0.2)
-	var last_point : RigidBody2D = null
-	var first_point : RigidBody2D = null
 	for i in range(amount):
 		var child : RigidBody2D = sbnode.instantiate()
 		add_child(child)
+		listPoints.append(child)
 		child.global_position = targetCore.global_position + Vector2(0,50).rotated(6.28 * i / amount)
 		for a in child.get_children():
 			a.scale = Vector2(0.1,0.1)
-		var joint = createJoint(targetCore, child)
-		listRadial.append(joint)
-		add_child(joint)
-		if(last_point):
-			var j2 = createJoint(child,last_point, 1000)
-			add_child(j2)
-			listRing.append(j2)
-		else:
-			first_point = child
-		if i == amount - 1:
-			var j2 = createJoint(first_point, child, 1000)
-			add_child(j2)
-			listRing.append(j2)
-			add_child(j2)
-			
-		last_point = child
-			
+	
+	for i in range(listPoints.size()):
+		for j in range(i, listPoints.size()):
+			add_child(createJoint(listPoints[i],listPoints[j]))
+	
+	
 	
 
-func createJoint(a : Node2D, b:Node2D, stiffness = 64.0, damping = 0.0) -> DampedSpringJoint2D:
+func createJoint(a : Node2D, b:Node2D, stiffness = 64.0, damping = 10.0) -> DampedSpringJoint2D:
 	var joint = RapierDampedSpringJoint2D.new()
 	joint.global_position = a.global_position
 	var delta = b.global_position - a.global_position
@@ -53,12 +37,3 @@ func createJoint(a : Node2D, b:Node2D, stiffness = 64.0, damping = 0.0) -> Dampe
 	joint.damping = damping
 	joint.rest_length = joint.length
 	return joint
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	for a : DampedSpringJoint2D in listRadial:
-		a.rest_length = radialDistance
-	
-	for a : DampedSpringJoint2D in listRing:
-		a.rest_length = ringDistance
-	pass
